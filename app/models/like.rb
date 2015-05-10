d84ceb049590aff3f3c351a479aa5951abc02b48
Buck Shlegeris <bshlegeris@paypal.com>
2015-05-09 23:51:28 -0700

    did the thing

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,6 +1,7 @@
 class Like < ActiveRecord::Base
   def self.likes_count(post)
-    Like.where(rebloggable_type: post.class.name, rebloggable_id: post.id).count
+    Like.where(rebloggable_type: post.class.name,
+      rebloggable_id: post.id).count
   end
 
   belongs_to :rebloggable, polymorphic: true
