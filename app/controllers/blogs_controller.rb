90d9ce098d9db7483344aaf97dc5c705c483a1d8
Buck Shlegeris <bshlegeris@paypal.com>
2015-05-10 01:10:38 -0700

    things i guess

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -1,7 +1,8 @@
 class BlogsController < ApplicationController
   def show
     @user = User.friendly.find(params[:id])
-    @posts = (@user.text_posts + @user.reblogs).sort_by { |x| -1 * x.created_at.to_i }
+    @posts = (@user.text_posts.where(:private => false) + 
+      @user.reblogs.where(:private => false)).sort_by { |x| -1 * x.created_at.to_i }
     render :show
   end
 end
