b425c230bbbd8945d7aaaed58d96da23d7f55adb
Buck Shlegeris <bshlegeris@paypal.com>
2015-05-09 22:58:43 -0700

    likes count is done

--- app/models/reblog.rb
+++ app/models/reblog.rb
@@ -1,11 +1,16 @@
 class Reblog < ActiveRecord::Base
   has_many :reblogs, as: :rebloggable
-  belongs_to :rebloggable, polymorphic: true
+
+  has_many :likes, as: :rebloggable
+
   belongs_to :user
 
   def reblog_descendents
     reblogs.length + reblogs.map(&:reblog_descendents).sum
   end
+
+
+  belongs_to :rebloggable, polymorphic: true
 
   def get_condensed_body
     # Remove all quotes at the start. Replace quotes in the middle with [...].
--- app/models/text_post.rb
+++ app/models/text_post.rb
@@ -1,11 +1,11 @@
 class TextPost < ActiveRecord::Base
   has_many :reblogs, as: :rebloggable
 
+  has_many :likes, as: :rebloggable
+
   belongs_to :user
 
   def reblog_descendents
     reblogs.length + reblogs.map(&:reblog_descendents).sum
   end
-
-  
 end
