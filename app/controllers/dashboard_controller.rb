013a5aad04c29710dcaf2776944a3dd48de8ea67
Buck Shlegeris <bshlegeris@paypal.com>
2015-05-09 17:34:32 -0700

    should work now I hope

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,10 @@
 class DashboardController < ApplicationController
   def dashboard
-    @posts = current_user.interesting_posts
+    if current_user
+      @posts = current_user.interesting_posts
+    else
+      @posts = TextPost.all.sort_by { |x| -1 * x.created_at.to_i }
+    end
 
     if params[:reblog]
       parent_class = { "TextPost" => TextPost, "Reblog" => Reblog }[params[:reblog_type]]
