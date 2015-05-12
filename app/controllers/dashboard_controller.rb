2c057ed73c8cd1a794b56dc9d284e8e295233b19
Buck Shlegeris <bshlegeris@paypal.com>
2015-05-11 20:10:18 -0700

    fixed the order of posts

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,7 +3,7 @@ class DashboardController < ApplicationController
     if current_user
       @posts = current_user.interesting_posts.take(30)
     else
-      @posts = TextPost.order("created_at ASC").limit(30)
+      @posts = TextPost.order("created_at DESC").limit(30)
     end
 
     if params[:reblog]
