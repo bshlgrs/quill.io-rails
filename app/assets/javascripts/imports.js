9cbd4dba50e5a4b8cdb1d8d26126b64da83baed2
Buck Shlegeris <bshlegeris@paypal.com>
2015-05-24 14:51:05 -0700

    markdown!

--- app/assets/javascripts/imports.js
+++ app/assets/javascripts/imports.js
@@ -1,1 +1,12 @@
 var ReactCSSTransitionGroup = React.addons.CSSTransitionGroup;
+
+marked.setOptions({
+  renderer: new marked.Renderer(),
+  gfm: true,
+  tables: true,
+  breaks: false,
+  pedantic: false,
+  sanitize: true,
+  smartLists: true,
+  smartypants: false
+});
