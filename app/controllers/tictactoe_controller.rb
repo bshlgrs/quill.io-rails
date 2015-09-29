c1d3eaaf095353026f0ecfbb6fbd46da5a83741e
Buck Shlegeris <bshlegeris@paypal.com>
2015-09-29 14:30:03 -0700

    worse ruby, maybe it works though?

--- app/controllers/tictactoe_controller.rb
+++ app/controllers/tictactoe_controller.rb
@@ -19,3 +19,106 @@ class TictactoeController < ApplicationController
     end
   end
 end
+
+class Board
+  attr_accessor :grid
+
+  def initialize(grid)
+    @grid = grid
+  end
+
+  def self.from_string(string)
+    # todo: error handling
+
+    return nil unless string.length == 9
+
+    grid = [0, 1, 2].map do |row_index|
+      [0, 1, 2].map do |col_index|
+        case string[row_index * 3 + col_index]
+        when "x"
+          :cross
+        when "o"
+          :naught
+        when " "
+          nil
+        else
+          return nil
+        end
+      end
+    end
+
+    Board.new(grid)
+  end
+
+  def to_wave_string
+    flattened_grid.map do |x|
+      case x
+      when :cross
+        "x"
+      when :naught
+        "o"
+      when nil
+        " "
+      end
+    end.join("")
+  end
+
+  def flattened_grid
+    @grid.flatten
+  end
+
+  def count_mark(mark)
+    flattened_grid.count(mark)
+  end
+
+  def is_valid?
+    (count_mark(:naught) - count_mark(:cross)).abs < 2
+  end
+
+  def plausibly_the_turn_of_o?
+    # what do I do if the board is invalid?
+    count_mark(:naught) == count_mark(:cross) || count_mark(:naught) == count_mark(:cross) - 1
+  end
+
+  def make_move_for_o
+    [0, 1, 2].each do |row|
+      [0, 1, 2].each do |col|
+        if @grid[row][col].nil?
+          @grid[row][col] = :naught
+          return
+        end
+      end
+    end
+  end
+
+  def make_move(piece, x, y)
+    if @grid[x][y] == nil
+      string = grid.to_wave_string
+      string[x + y * 3] = piece == :naught ? "o" : x
+      Board.from_string(string)
+    else
+      raise new RuntimeException
+    end
+  end
+
+  def is_over?
+    ! flattened_grid.contains?(nil)
+  end
+
+  def winner
+    # If two players have won, the output of this function is undefined.
+    horizontals = @grid
+    verticals = @grid.transpose
+
+    up_diagonal = [@grid[0][0], @grid[1][1], @grid[2][2]]
+    down_diagonal = [@grid[2][0], @grid[1][1], @grid[0][2]]
+
+    lines = horizontals + verticals + up_diagonal + down_diagonal
+    
+    lines.each do |line|
+      return :nought if line.all? { |x| x == :nought }
+      return :cross if line.all? { |x| x == :cross }
+    end
+  end
+end
+
